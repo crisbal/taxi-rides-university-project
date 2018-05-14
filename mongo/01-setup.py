98e17299344a454c4485694ff4eefb81e36b64bb
Cristian Baldi <bld.cris.96@gmail.com>
2018-05-14 14:52:18 +0200

    Drop database and create user in mongo setup

--- mongo/01-setup.py
+++ mongo/01-setup.py
@@ -2,11 +2,14 @@ from pymongo import MongoClient
 
 if __name__ == '__main__':
     client = MongoClient('localhost', 27017, username='root', password='password')
+    print("Dropping database")
+    client.drop_database("taxiRides")
     db = client["taxiRides"]
-    
-    collections = ["rides"]
+    db.add_user('root', 'password', roles=[{'role':'readWrite','db':'taxiRides'}])
+    #db.drop()
+    """collections = ["rides"]
     for collection in collections:
         print(f"Dropping collection {collection}")
         db[collection].drop()
-        print("\tDropped")
-        
+        print("\tDropped")"""
+    
